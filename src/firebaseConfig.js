ec3a900e5e0708732a1ae8817528659f70186a88
Rumaisa Naveed <rumaisanaved@gmail.com>
2024-10-26 22:47:17 +0500

    Initiazlied the project with react and firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBb0M1ZV5pJ1r9a6nOVUgnJZTm_LI0x3PE",
+  authDomain: "stationary-simplified.firebaseapp.com",
+  projectId: "stationary-simplified",
+  storageBucket: "stationary-simplified.appspot.com",
+  messagingSenderId: "473448115303",
+  appId: "1:473448115303:web:2eefab1e10505a7d7378c2",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
