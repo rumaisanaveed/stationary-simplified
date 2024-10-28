984c01747cb20f167e138bd0682af724d2cd2dac
Hammad Hussain <connect.hammadhussain@gmail.com>
2024-10-28 11:08:50 +0500

    Initial Structure of Card

---
+++ src/TemplateCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const TemplateCard = () => {
+  return (
+    <div className='w-min-[250px] h-min-[250px] flex flex-col justify-between items-end'>
+      <div className="flex flex-col-gap-1">
+        <div className='bg-white p-1'>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
