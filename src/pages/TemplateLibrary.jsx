9f2f733bd2cb1df05372a697de5b27974ea44ae1
Rumaisa Naveed <rumaisanaved@gmail.com>
2024-10-28 22:19:55 +0500

    Merged the changes from the other branches

--- src/pages/TemplateLibrary.jsx
+++ src/pages/TemplateLibrary.jsx
@@ -5,11 +5,13 @@ import { TemplateCard } from "../components/TemplateCard";
 export const TemplateLibrary = () => {
   return (
     <MainLayout>
-     <div className="flex flex-col p-5 gap-4 bg-[#f4f4f4]">
-      <h1 className="text-xl ">{headinng}</h1>
-      <div className="flex items-center flex-wrap gap-4"> {Array.from({length : 10}, () => (
-        <TemplateCard />     
-      ))}</div>
+      <div className="flex flex-col p-5 gap-4 bg-[#f4f4f4]">
+        <h1 className="text-xl ">Template Library</h1>
+        <div className="flex items-center flex-wrap gap-4">
+          {Array.from({ length: 10 }, () => (
+            <TemplateCard />
+          ))}
+        </div>
       </div>
     </MainLayout>
   );
